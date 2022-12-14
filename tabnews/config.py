34cbf0e23fd4f35a313ebe29b064ae59c9bae1b6
Gustavo Santana <gustavo.santana6840@gmail.com>
2022-12-14 21:06:22 +0000

    FIX: `config.py` is removed from `.gitignore`

---
+++ tabnews/config.py
@@ -0,0 +1,9 @@
+class Config:
+    #HOST = 'https://www.tabnews.com.br'
+    HOST = 'https://tabnews-4lk1vytlv-tabnews.vercel.app'
+
+    LOGIN_URL = HOST+'/api/v1/sessions'
+    CONTENTS_URL = HOST+'/api/v1/contents'
+    USER_DATA_URL = HOST+'/api/v1/user'
+
+
