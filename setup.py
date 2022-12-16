84823208d5747e36dca962a00c97cd1903832c18
Gustavo Santana <gustavo.santana6840@gmail.com>
2022-12-16 01:44:10 +0000

    ADDED: `setup.py` - To install the lib locally

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tabnews",
+    version="1.0.0",
+    author="Gustavo Santana",
+    author_email="sowlfie@gmail.com",
+    description='Python library for communication via API to the website: "TabNews.com.br"',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gustavosta/Tabnews.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
